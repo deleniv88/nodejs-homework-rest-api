e05d23f32a8ac83daea9df0dbdc2421a592b86ad
Andrew <deleniv08@gmail.com>
2023-01-28 09:08:52 +0100

    update middelwares auth

--- middelwares/index.js
+++ middelwares/index.js
@@ -1,12 +1,45 @@
+// const { Unauthorized } = require("http-errors");
+// const jwt = require("jsonwebtoken");
+// const { User } = require("../models/users");
+
 function validateBody(schema) {
     return (req, res, next) => {
         const { error } = schema.validate(req.body);
-
         if (error) {
-            return next(res.status(400).json({ message: error.message }))
+            return next(res.status(400).json({
+                message: "There is an error in email or password from Joi validation library or from another library"
+            }));
         }
-        return next()
+        return next();
     }
 }
 
-module.exports = validateBody
+// async function auth(req, res, next) {
+//     const authHeader = req.headers.authorization || "";
+//     const [type, token] = authHeader.split(" ");
+
+//     try {
+//         const { id } = jwt.verify(token, process.env.JWT_SECRET);
+//         const user = await User.findById(id);
+//         req.user = user;
+
+//     if (type !== 'Bearer') {
+//         throw Unauthorized("Not authorized");
+//     }
+
+//     if (!token || !user.token)  {
+//         throw Unauthorized("Not authorized");
+//     }
+
+//     } catch (error) {
+//         if (error.name === "TokenExpiredError" || error.name === "JsonWebTokenError") {
+//             throw Unauthorized("Not authorized");
+//         } throw error;
+//     }
+//     next();
+// }
+
+module.exports = {
+    validateBody,
+    // auth
+}
