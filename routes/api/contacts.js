4e5471e28b32ceed89c4cc7c47634507f7daa3f8
Andrew <deleniv08@gmail.com>
2023-01-28 08:10:33 +0100

    hw-04-auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const { getAllContacts, getContact, createContact, deleteContact, updContact, contactStatus} = require('../../controllers/contacts.controller');
 
-const validateBody = require('../../middelwares');
+const {validateBody} = require('../../middelwares/index');
 const addContactSchema = require('../../schemas/contacts');
 
 router.get('/', getAllContacts)
-router.get('/:contactId', getContact)
+router.get('/:contactId', getContact);
 router.post('/', validateBody(addContactSchema), createContact)
 router.delete('/:contactId', deleteContact)
 router.put('/:contactId', validateBody(addContactSchema), updContact)
-router.patch('/:contactId/favorite', contactStatus)
+router.patch('/:contactId/favorite', contactStatus);
 
 module.exports = router
 
